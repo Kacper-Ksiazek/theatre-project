901a2a0be7a6d730586470468edd24bf12c44fe3
Kacper Książek <58363032+Kacper-Ksiazek@users.noreply.github.com>
2023-12-23 21:24:09 +0100

    Layout / Nawigacja- responsywnosc Close #7

---
+++ javascript/layout/nawigacja/wersja_mobilna.js
@@ -0,0 +1,28 @@
+// Stale
+const SELECTORS = {
+    OPEN_MOBILE_MENU_BUTTON: "#open-mobile-menu",
+    MENU: "#main-menu-actions-wrapper",
+};
+
+const OPENED_CSS_CLASS = "opened";
+
+//
+//
+//
+// Pobranie przycisku otwierającego menu mobilne
+const button = document.querySelector(SELECTORS.OPEN_MOBILE_MENU_BUTTON);
+if (!button) throw new Error(`Nie znaleziono przycisku otwierającego menu mobilne! Szukany selektor to: ${SELECTORS.OPEN_MOBILE_MENU_BUTTON}`);
+
+// Pobranie menu mobilnego
+const menu = document.querySelector(SELECTORS.MENU);
+if (!menu) throw new Error(`Nie znaleziono menu mobilnego! Szukany selektor to: ${SELECTORS.MENU}`);
+
+//
+//
+//
+// Dodanie obsługi kliknięcia w przycisk
+button.addEventListener("click", () => {
+    [menu, button].forEach((element) => {
+        element.classList.toggle(OPENED_CSS_CLASS);
+    });
+});

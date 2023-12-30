38c4bc0a68e4947916f178c3d528edd6240396bf
Kacper Książek <58363032+Kacper-Ksiazek@users.noreply.github.com>
2023-12-30 21:54:09 +0100

    Poprawka skryptu do obslugi menu mobilnego

--- javascript/layout/nawigacja/wersja_mobilna.js
+++ javascript/layout/nawigacja/wersja_mobilna.js
@@ -1,28 +1,30 @@
-// Stale
-const SELECTORS = {
-    OPEN_MOBILE_MENU_BUTTON: "#open-mobile-menu",
-    MENU: "#main-menu-actions-wrapper",
-};
+(() => {
+    // Stale
+    const SELECTORS = {
+        OPEN_MOBILE_MENU_BUTTON: "#open-mobile-menu",
+        MENU: "#main-menu-actions-wrapper",
+    };
 
-const OPENED_CSS_CLASS = "opened";
+    const OPENED_CSS_CLASS = "opened";
 
-//
-//
-//
-// Pobranie przycisku otwierającego menu mobilne
-const button = document.querySelector(SELECTORS.OPEN_MOBILE_MENU_BUTTON);
-if (!button) throw new Error(`Nie znaleziono przycisku otwierającego menu mobilne! Szukany selektor to: ${SELECTORS.OPEN_MOBILE_MENU_BUTTON}`);
+    //
+    //
+    //
+    // Pobranie przycisku otwierającego menu mobilne
+    const button = document.querySelector(SELECTORS.OPEN_MOBILE_MENU_BUTTON);
+    if (!button) throw new Error(`Nie znaleziono przycisku otwierającego menu mobilne! Szukany selektor to: ${SELECTORS.OPEN_MOBILE_MENU_BUTTON}`);
 
-// Pobranie menu mobilnego
-const menu = document.querySelector(SELECTORS.MENU);
-if (!menu) throw new Error(`Nie znaleziono menu mobilnego! Szukany selektor to: ${SELECTORS.MENU}`);
+    // Pobranie menu mobilnego
+    const menu = document.querySelector(SELECTORS.MENU);
+    if (!menu) throw new Error(`Nie znaleziono menu mobilnego! Szukany selektor to: ${SELECTORS.MENU}`);
 
-//
-//
-//
-// Dodanie obsługi kliknięcia w przycisk
-button.addEventListener("click", () => {
-    [menu, button].forEach((element) => {
-        element.classList.toggle(OPENED_CSS_CLASS);
+    //
+    //
+    //
+    // Dodanie obsługi kliknięcia w przycisk
+    button.addEventListener("click", () => {
+        [menu, button].forEach((element) => {
+            element.classList.toggle(OPENED_CSS_CLASS);
+        });
     });
-});
+})();
